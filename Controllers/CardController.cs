49c722b6f47846d04d7468b6eeac69bf5ac747a2
Mourad <mouradalfadil@gmail.com>
2023-03-21 14:32:09 -0400

    expanded backend functions pls dont brick

--- Controllers/CardController.cs
+++ Controllers/CardController.cs
@@ -8,7 +8,7 @@ namespace Final_Project___Magic_App.Controllers
     [ApiController]
     public class CardController : ControllerBase
     {
-        
+        DeckDbContext dbcontext = new DeckDbContext();
 
         //requesting all cards from scryfall, Probably not going to use this but adding it in for testing purposes
 
@@ -16,6 +16,33 @@ namespace Final_Project___Magic_App.Controllers
         public List<CardModel> getAllCards()
         {
             return new List<CardModel>();
+        }
+
+        //method to add deck
+        //[HttpGet("CreateDeck")]
+        //public DeckTable createDeck(string deckName, CardTable cards, )
+        //{
+        //    DeckTable newDeck = new DeckTable()
+        //    {
+        //        DeckName = deckName,
+        //        Uid = userid
+        //    };
+        //    return dbcontext.DeckTables.FirstOrDefault(u => u.Uid == UID);
+        //    //&&(u=>u.DeckName == deckName))
+        //}
+
+        //method to get deck by id
+        [HttpGet("GetDeckByUser")]
+        public DeckTable searchDeckByID(string UID)
+        {
+            return dbcontext.DeckTables.FirstOrDefault(u => u.Uid == UID);
+            //&&(u=>u.DeckName == deckName))
+        }
+        //method to get all decks by userID (google)
+        [HttpGet("GetAllDeckByUser")]
+        public List<DeckTable> allDecksByID(string UID)
+        {
+            return dbcontext.DeckTables.Where(d => d.Uid == UID).ToList();
         }
 
         //searching cards by the exact name note that this should be case insensitive
